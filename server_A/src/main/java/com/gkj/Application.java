8e39391d688aa9b5879ea55b6b1056bd082770dc
guokunjin <1255903774@qq.com>
2018-10-17 15:14:19 +0800

    准备工作

---
+++ server_A/src/main/java/com/gkj/Application.java
@@ -0,0 +1,17 @@
+package com.gkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author guokunjin
+ * @Version 1.0
+ * @Date 13:52 2018/10/17
+ * @Modified By:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ server_A/src/main/java/com/gkj/controller/RestApi.java
@@ -0,0 +1,28 @@
+package com.gkj.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author guokunjin
+ * @Version 1.0
+ * @Date 13:46 2018/10/17
+ * @Modified By:
+ */
+@RestController
+public class RestApi {
+    @Value("${profile}")
+    String profile;
+    @GetMapping("version")
+    public Map restTest(){
+        Map map=new HashMap();
+        map.put("appName","SpringBoot Server 1");
+        map.put("version","1.0.SNAPSHOT");
+        map.put("profile",profile);
+        return map;
+    }
+}
